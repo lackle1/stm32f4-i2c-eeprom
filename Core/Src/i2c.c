f10c29250147c3fb162ccdaecbd3ab15c3125ad7
lackle1 <lachlankeane014@gmail.com>
2025-10-22 11:16:33 +1100

    Changed I2C code to use macros

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -18,8 +18,8 @@
 void I2C_config(I2C_TypeDef *i2c) {
 
     // Enable GPIO and I2C clocks
-    RCC->APB1ENR |= 1<<21;  // I2C
-    RCC->AHB1ENR |= 1<<1;   // GPIOB
+    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;  // I2C
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;   // GPIOB
 
     // Configure pins PB8 and PB9 to use alternate functions
     GPIOB->MODER |= 2<<16 | 2<<18;      // Alternative function mode (0b10)
@@ -31,8 +31,8 @@ void I2C_config(I2C_TypeDef *i2c) {
     GPIOB->AFR[1] |= 4<<0 | 4<<4;       // Set each to AF4 (I2C)
 
     // Reset I2C
-    i2c->CR1 |= 1<<15;
-    i2c->CR1 &= ~(1<<15);
+    i2c->CR1 |= I2C_CR1_SWRST;
+    i2c->CR1 &= ~I2C_CR1_SWRST;
 
     i2c->CR2 |= 42;                    // Set frequency to the APB1 frequency (MHz)
 
@@ -41,7 +41,7 @@ void I2C_config(I2C_TypeDef *i2c) {
     i2c->CCR |= 210;                   // Calculated from reference manual. Datasheet values for clock times were missing so I looked at similar boards.
 
     i2c->TRISE |= 46;                  // Configure TRISE
-    i2c->CR1 |= 1<<0;                  // Enable peripheral
+    i2c->CR1 |= I2C_CR1_PE;                  // Enable peripheral
 }
 
 /**
@@ -52,9 +52,9 @@ void I2C_config(I2C_TypeDef *i2c) {
  * @return @c NULL
  **/
 void I2C_start(I2C_TypeDef *i2c) {
-    i2c->CR1 |= 1<<8;               // Enable ACK
-    i2c->CR1 |= 1<<8;               // Set START bit
-    while (!(i2c->SR1 & 1<<0));     // Wait for the SB bit to be set
+    i2c->CR1 |= I2C_CR1_ACK;                // Enable ACK
+    i2c->CR1 |= I2C_CR1_START;              // Set START bit
+    while (!(i2c->SR1 & I2C_SR1_SB));       // Wait for the SB bit to be set
 }
 
 /**
@@ -67,7 +67,7 @@ void I2C_start(I2C_TypeDef *i2c) {
  **/
 void I2C_sendAddress(I2C_TypeDef *i2c, uint8_t addr) {
     i2c->DR = addr;
-    while (!(i2c->SR1 & 1<<1));         // Wait for ADDR bit to be set
+    while (!(i2c->SR1 & I2C_SR1_ADDR));         // Wait for ADDR bit to be set
     (void)(i2c->SR1 | i2c->SR2);        // Read status registers to clear ADDR
 }
 
@@ -79,7 +79,7 @@ void I2C_sendAddress(I2C_TypeDef *i2c, uint8_t addr) {
  * @return @c NULL
  **/
 void I2C_stop(I2C_TypeDef *i2c) {
-    i2c->CR1 |= 1<<9;   // Sets stop bit
+    i2c->CR1 |= I2C_CR1_STOP;   // Sets stop bit
 }
 
 /**
@@ -95,12 +95,12 @@ void I2C_stop(I2C_TypeDef *i2c) {
 void I2C_write(I2C_TypeDef *i2c, uint8_t addr, uint8_t *data, uint8_t size) {
 
     for (int i = 0; i < size; i++) {
-        while (!(i2c->SR1 & 1<<7));    // Wait for TxE bit to be set (data register empty)
+        while (!(i2c->SR1 & I2C_SR1_TXE));    // Wait for TxE bit to be set (data register empty)
         i2c->DR = data[i];
         size--;
     }
 
-    while (!(i2c->SR1 & 1<<2));        // Wait for BTF to be set (byte transfer finished)
+    while (!(i2c->SR1 & I2C_SR1_BTF));        // Wait for BTF to be set (byte transfer finished)
 }
 
 /**
@@ -117,37 +117,37 @@ void I2C_read(I2C_TypeDef *i2c, uint8_t addr, uint8_t *buf, uint8_t size) {
 
     if (size == 1) {
         i2c->DR = addr;
-        while (!(i2c->SR1 & 1<<1));         // Wait for ADDR bit to be set
+        while (!(i2c->SR1 & I2C_SR1_ADDR));             // Wait for ADDR bit to be set
 
-        i2c->CR1 &= ~(1<<10);               // Disable ACK
-        i2c->CR1 |= 1<<11;                  // Set POS bit
+        i2c->CR1 &= ~(I2C_CR1_ACK);                     // Disable ACK
+        i2c->CR1 |= I2C_CR1_POS;                        // Set POS bit
         
-        i2c->CR1 |= 1<<9;                   // STOP
+        i2c->CR1 |= I2C_CR1_STOP;                       // STOP
 
-        while (!(i2c->SR1 & 1<<6));         // Wait until RxNE is set (data register not empty)
+        while (!(i2c->SR1 & I2C_SR1_RXNE));             // Wait until RxNE is set (data register not empty)
         buf[0] = i2c->DR;
     }
     else {
         uint8_t remaining = size;
 
         while (remaining > 2) {
-            while (!(i2c->SR1 & 1<<6));         // Wait until RxNE is set (data register not empty)
+            while (!(i2c->SR1 & I2C_SR1_RXNE));         // Wait until RxNE is set (data register not empty)
             buf[size - remaining] = i2c->DR;
-            i2c->CR1 |= 1<<10;                  // Enable ACK (to acknowledge data has been received)
+            i2c->CR1 |= I2C_CR1_ACK;                    // Enable ACK (to acknowledge data has been received)
             remaining--;
         }
 
         // Read second last byte
-        while (!(i2c->SR1 & 1<<6));             // Wait until RxNE is set (data register not empty)
+        while (!(i2c->SR1 & I2C_SR1_RXNE));             // Wait until RxNE is set (data register not empty)
         buf[size - remaining] = i2c->DR;
 
-        i2c->CR1 &= ~(1<<10);                   // Disable ACK
+        i2c->CR1 &= ~I2C_CR1_ACK;                       // Disable ACK
         I2C_stop(i2c);
 
         remaining--;
 
         // Read last byte
-        while (!(i2c->SR1 & 1<<6));         // Wait until RxNE is set (data register not empty)
+        while (!(i2c->SR1 & I2C_SR1_RXNE));             // Wait until RxNE is set (data register not empty)
         buf[size - remaining] = i2c->DR;
     }
 }
